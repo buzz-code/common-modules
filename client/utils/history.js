736d3b3843931bab89649a3cccf0bc952adcac9f
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-03 19:37:39 +0300

    add utils folder to common

---
+++ client/utils/history.js
@@ -0,0 +1,8 @@
+import { createBrowserHistory as createHistory } from 'history';
+import PACKAGE from '../../../package.json';
+
+// a singleton history object
+const history = createHistory({
+  basename: process.env.NODE_ENV !== 'development' ? `/${PACKAGE.name}/` : undefined,
+});
+export default history;
