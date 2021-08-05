6d0f05a92eaadaeabba4517f73302c3bb98f9191
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-05 23:25:25 +0300

    fix typo

--- server/config/directory.js
+++ server/config/directory.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var rootPath = path.normalize(__dirname, '..', '..', '..')
+var rootPath = path.join(__dirname, '..', '..', '..')
 
 const directory = {
     root: rootPath,
