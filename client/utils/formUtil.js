61df8ccb9ceff313680f8e57bb6cbec720357bb9
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-05 22:56:34 +0300

    add support for idFIeld

--- client/utils/formUtil.js
+++ client/utils/formUtil.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 
-export const getPropsForAutoComplete = (field, list) => ({
-  render: (rowData) => <span>{list.find((item) => item.id == rowData[field])?.name}</span>,
+export const getPropsForAutoComplete = (field, list, idField = 'id') => ({
+  render: (rowData) => <span>{list.find((item) => item[idField] == rowData[field])?.name}</span>,
   editComponent: (props) => (
     <Autocomplete
       size="small"
       options={list}
-      getOptionLabel={(option) => option.name || list.find((item) => item.id == props.value).name}
-      getOptionSelected={(option, value) => option.id == value}
+      getOptionLabel={(option) => option.name || list.find((item) => item[idField] == props.value).name}
+      getOptionSelected={(option, value) => option[idField] == value}
       value={props.value}
       renderInput={(params) => {
         return <TextField {...params} fullWidth />;
       }}
-      onChange={(e, value) => props.onChange(value && value.id)}
+      onChange={(e, value) => props.onChange(value && value[idField])}
     />
   ),
 });
--- server/utils/common.js
+++ server/utils/common.js
@@ -1,6 +1,6 @@
-export function getListFromTable(table, user_id) {
+export function getListFromTable(table, user_id, idField = 'id') {
     return new table().where({ user_id })
-        .query({ select: ['id', 'name'] })
+        .query({ select: [idField, 'name'] })
         .fetchAll()
         .then(result => result.toJSON());
 }
