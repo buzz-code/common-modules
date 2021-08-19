a064ed6783616707eec9e261fec24c7ab41b5dc1
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-19 19:09:28 +0300

    move webpack to common & and custom domain support

--- client/utils/history.js
+++ client/utils/history.js
@@ -1,8 +1,10 @@
 import { createBrowserHistory as createHistory } from 'history';
 import PACKAGE from '../../../package.json';
 
+const isAppendPackageName = process.env.NODE_ENV !== 'development' && PACKAGE.customDomain === false;
+
 // a singleton history object
 const history = createHistory({
-  basename: process.env.NODE_ENV !== 'development' ? `/${PACKAGE.name}/` : undefined,
+  basename: isAppendPackageName ? `/${PACKAGE.name}/` : undefined,
 });
 export default history;
