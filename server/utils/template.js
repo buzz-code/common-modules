9fabc8dfa2258b985d9e395657d1ce5e3f4137cc
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-22 21:39:11 +0300

    no header or footer for pdf

--- server/utils/template.js
+++ server/utils/template.js
@@ -17,12 +17,12 @@ export function getPdfStreamFromHtml(html, options) {
     return new Promise((resolve, reject) => {
         let pdfOptions = {
             format: 'A4',
-            header: {
-                height: '7mm'
-            },
-            footer: {
-                height: '7mm',
-            },
+            // header: {
+            //     height: '7mm'
+            // },
+            // footer: {
+            //     height: '7mm',
+            // },
             ...options
         };
         pdf.create(html, pdfOptions).toStream(function (err, stream) {
