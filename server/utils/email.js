2c970590d96d41c8e94b956494e7c8444ccdf8d3
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-10-30 21:18:45 +0300

    update email parse

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,6 +1,10 @@
 export async function getAndParseExcelEmail(req, res){
     res.send('success')
+    const keys = Object.keys(req.body);
+    console.log('keys: ', keys)
     const fromAddress = req.body.from.value;
     console.log('from: ', fromAddress);
+    const attachments = req.body.attachments;
+    console.log('attachment length: ', attachments.length)
     return {}
 }
