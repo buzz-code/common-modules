7add9686fa665ef801457f33637eea523ce9c5b9
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-10-30 21:44:11 +0300

    update email parse

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,10 +1,26 @@
-export async function getAndParseExcelEmail(req, res){
-    res.send('success')
-    const keys = Object.keys(req.body);
-    console.log('keys: ', keys)
+import XLSX from 'xlsx';
+
+export async function getAndParseExcelEmail(req, res) {
+    res.send('success');
     const fromAddress = req.body.from.value;
-    console.log('from: ', fromAddress);
+    console.log('got email from: ', fromAddress);
     const attachments = req.body.attachments;
-    console.log('attachment length: ', attachments.length)
-    return {}
+
+    if (attachments.length == 1) {
+        console.log('got attachment: ', attachments[0].filename, attachments[0].contentType);
+        const buffer = attachments[0].content.data;
+        const wb = XLSX.read(buffer, { type: 'array' });
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+        /* Update state */
+        console.log(data);
+
+        return [];
+    } else {
+        console.log('no data was received');
+        return [];
+    }
 }
