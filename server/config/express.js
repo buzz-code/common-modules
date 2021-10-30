99fb5c825cc5fc7d83857589ff0bc90f2c0d8b76
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-10-30 21:42:17 +0300

    update express configuration and remove body parser

--- server/config/express.js
+++ server/config/express.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -23,8 +22,8 @@ app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(methodOverride());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(morgan('dev'));
 app.use(express.static(constant.assetsDir));
 
