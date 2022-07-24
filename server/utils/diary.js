fd6a725c33cba732ff66a90e0e6efc191c078f8d
buzz-code <72219643+buzz-code@users.noreply.github.com>
2022-07-24 13:18:08 +0300

    update diary util

--- server/utils/diary.js
+++ server/utils/diary.js
@@ -1,11 +1,15 @@
 import { getJewishDate, formatJewishDateHebrew } from 'jewish-dates-core';
 
+function getDayFromLessonKey(lesson_key) {
+    return lesson_key?.split('_')?.[2];
+}
+
 export function getDatesFromDiaryData(diaryData) {
-    return Object.fromEntries(diaryData.map(({ lesson_key, lesson_date }) => ([lesson_key, lesson_date])));
+    return Object.fromEntries(diaryData.map(({ lesson_key, lesson_date }) => ([getDayFromLessonKey(lesson_key), lesson_date])));
 }
 
 export function getSubtituteFromDiaryData(diaryData) {
-    return Object.fromEntries(diaryData.map(({ lesson_key, is_substitute }) => ([lesson_key, is_substitute])));
+    return Object.fromEntries(diaryData.map(({ lesson_key, is_substitute }) => ([getDayFromLessonKey(lesson_key), is_substitute])));
 }
 
 export function formatJewishDates(groupData) {
@@ -14,6 +18,22 @@ export function formatJewishDates(groupData) {
     }
 }
 
-export function getLessonsByLessonCount(lesson_count) {
-    return [...Array(lesson_count)].map((_, index) => `lesson_date_${index + 1}`);
+export function getDaysByLessonCount(day_count, lesson_count) {
+    if (!day_count) {
+        return getDaysByLessonCount(lesson_count, 1);
+    }
+
+    return [...Array(day_count)]
+        .map((_, dayIndex) => ({
+            key: dayIndex + 1,
+            lessons: [...Array(lesson_count)]
+                .map((_, lessonIndex) => `lesson_date_${dayIndex + 1}_${lessonIndex + 1}`)
+        }));
+}
+
+export function getNewLessonKey(lesson_key) {
+    if (lesson_key?.split('_')?.length === 3) {
+        return lesson_key + '_1';
+    }
+    return lesson_key;
 }
