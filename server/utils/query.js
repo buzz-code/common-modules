73b410901df686f7e211c5880d534e9d7e1693fa
buzz-code <72219643+buzz-code@users.noreply.github.com>
2022-09-15 21:50:39 +0300

    add function to get minimal model

--- server/utils/query.js
+++ server/utils/query.js
@@ -1,7 +1,17 @@
+import bookshelf from "../config/bookshelf";
+
 export function getCountFromTable(table, user_id, filter = {}, groupBy = 'id') {
     return new table().where({ user_id })
         .where(filter)
         .query()
         .countDistinct({ count: groupBy })
         .then(res => res[0].count);
+}
+
+const models = {};
+export function getMinimalModel(tableName) {
+    if (!models[tableName]) {
+        models[tableName] = bookshelf.model(tableName, { tableName });
+    }
+    return models[tableName];
 }
