d08ea9b9f5bacbf88af411353ae00f4394cc409c
buzz-code <72219643+buzz-code@users.noreply.github.com>
2022-10-31 16:43:39 +0200

    add reply-to parameter to sendEmail func

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,11 +1,11 @@
 import sgMail from '@sendgrid/mail';
 
-export async function sendEmail(toAddress, fromAddress, subject, text, html) {
+export async function sendEmail(toAddress, fromAddress, subject, text, html, replyTo = 'hadasa.schechter@gmail.com') {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const msg = {
         to: toAddress,
         from: fromAddress,
-        replyTo: 'hadasa.schechter@gmail.com',
+        replyTo: replyTo,
         subject: subject,
         text: text,
         html: html,
