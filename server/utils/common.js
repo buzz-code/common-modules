4b5fafed8c298548a117cf3ff1ce283619df0521
buzz-code <72219643+buzz-code@users.noreply.github.com>
2023-08-30 22:26:15 +0300

    add ability to filter lists

--- server/utils/common.js
+++ server/utils/common.js
@@ -1,5 +1,6 @@
-export function getListFromTable(table, user_id, idField = 'id') {
+export function getListFromTable(table, user_id, idField = 'id', filter = {}) {
     return new table().where({ user_id })
+        .where(filter)
         .query({ select: [idField, 'name'] })
         .fetchAll()
         .then(result => result.toJSON());
