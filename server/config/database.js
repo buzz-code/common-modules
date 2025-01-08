35c9b409c3ceb86cea10275c6d4acf1174ad0ddc
Hadasa Schechter <hadasa.schchter@vcita.com>
2025-01-08 11:11:26 +0200

    update database configuration to use environment variables for filename and migrations path

--- server/config/database.js
+++ server/config/database.js
@@ -10,10 +10,12 @@ export default {
         database: process.env.DB_NAME,
         charset: 'utf8',
         socketPath: process.env.SOCKET_PATH,
+        filename: process.env.DB_FILENAME,
     },
+    useNullAsDefault: true,
     migrations: {
         tableName: 'migrations',
-        directory: process.cwd() + '/server/migrations',
+        directory: process.env.MIGRATIONS_PATH ?? process.cwd() + '/server/migrations',
     },
     debug: process.env.DB_DEBUG === 'true'
 };
